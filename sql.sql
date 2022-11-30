81b07a6ff59b06018f732923dfdee44764dd620c
Dev P <devashish16@gmail.com>
2022-11-30 15:38:47 +1100

    30/11/2022 2

--- sql.sql
+++ sql.sql
@@ -296,3 +296,19 @@ SELECT product_id
 , FIRST_VALUE(category) over (PARTITION BY cnt order by product_id) as category
 , name
 from base
+
+-- Spotify Streaming History
+WITH history AS (
+SELECT user_id, song_id, song_plays
+FROM songs_history
+UNION ALL
+SELECT user_id, song_id, COUNT(song_id) AS song_plays
+FROM songs_weekly
+WHERE listen_time <= '08/04/2022 23:59:59'
+GROUP BY user_id, song_id
+)
+
+SELECT user_id, song_id, SUM(song_plays) AS song_count
+FROM history
+GROUP BY user_id, song_id
+ORDER BY song_count DESC;
