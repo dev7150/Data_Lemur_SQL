6efc48c53c60aba2ee2a25a52a07cb516f9be5f2
Dev P <devashish16@gmail.com>
2022-12-20 14:28:40 +1100

    Recursion

--- sql.sql
+++ sql.sql
@@ -583,4 +583,81 @@ full outer join daily_pay b
 on a.user_id = b.user_id
 order by 1;
 
+-- 3-Topping Pizzas [McKinsey SQL Interview Question]
+SELECT p1.topping_name||','|| p2.topping_name ||','||p3.topping_name as pizza
+, (p1.ingredient_cost + p2.ingredient_cost + p3.ingredient_cost) as total_cost
+FROM pizza_toppings p1
+join pizza_toppings p2
+on p1.topping_name < p2.topping_name
+join pizza_toppings p3
+on p2.topping_name < p3.topping_name
+order by total_cost desc,pizza
+
+-- using recursion
+WITH RECURSIVE all_toppings AS (
+  SELECT
+  topping_name::VARCHAR,
+  ingredient_cost::DECIMAL AS total_cost,
+  1 AS topping_numbers
+FROM pizza_toppings
+
+UNION ALL
+
+SELECT
+  CONCAT(addon.topping_name, ',', anchor.topping_name) AS topping_name,
+  addon.ingredient_cost + anchor.total_cost AS total_cost,
+  topping_numbers + 1
+FROM 
+  pizza_toppings AS addon, 
+  all_toppings AS anchor
+WHERE anchor.topping_name < addon.topping_name
+)
+
+SELECT
+  STRING_AGG (single_topping, ',' ORDER BY single_topping) AS pizza,
+  total_cost
+FROM 
+  all_toppings, 
+  REGEXP_SPLIT_TO_TABLE(topping_name, ',') AS single_topping
+WHERE topping_numbers = 3
+GROUP BY topping_name, total_cost
+ORDER BY total_cost DESC, pizza;
+
+-- Using Array functions
+WITH RECURSIVE all_toppings AS (
+SELECT
+  topping_name::VARCHAR,
+  ingredient_cost::DECIMAL AS total_cost,
+  1 AS topping_numbers
+FROM pizza_toppings
+
+UNION ALL
+
+SELECT
+  CONCAT(addon.topping_name, ',', anchor.topping_name) AS topping_name,
+  addon.ingredient_cost + anchor.total_cost AS total_cost,
+  topping_numbers + 1
+FROM 
+  pizza_toppings AS addon,
+  all_toppings AS anchor
+WHERE anchor.topping_name < addon.topping_name
+), 
+arrange AS (
+SELECT
+  topping_name,
+  UNNEST(STRING_TO_ARRAY(topping_name, ',')) AS single_topping,
+  total_cost
+FROM all_toppings
+WHERE topping_numbers = 3
+)
+
+SELECT
+  STRING_AGG(single_topping, ',' ORDER BY single_topping) AS pizza,
+  total_cost
+FROM arrange
+GROUP BY topping_name, total_cost
+ORDER BY total_cost DESC, pizza;
+
+
+
 
